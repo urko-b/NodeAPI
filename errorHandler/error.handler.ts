907658dd95650ed7720dc7b8cb438e184d9e3730
elefantelimpio <lordguerrero94@gmail.com>
2019-02-17 18:34:30 +0100

    Refactor patch.handler.ts --> use async/await and handling i18n response errors

--- errorHandler/error.handler.ts
+++ errorHandler/error.handler.ts
@@ -1,10 +1,8 @@
 
-import * as i18n from 'i18n';
-
 export class ErrorHandler {
     public static handleE11000(error, res, next): void {
         if (error.name === 'MongoError' && error.code === 11000) {
-            next(new Error(i18n.__('There was a duplicate key error')));
+            next(new Error(res.__('There was a duplicate key error')));
         } else {
             next();
         }
