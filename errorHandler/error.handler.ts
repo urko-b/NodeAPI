ab3078c4cb5ac21f23f86d6b4a683de076cd0716
elefantelimpio <lordguerrero94@gmail.com>
2019-02-17 17:10:06 +0100

    Resolve node-restful issu using patch-package. Added i18n --> TODO: fill languages messages

--- errorHandler/error.handler.ts
+++ errorHandler/error.handler.ts
@@ -1,8 +1,10 @@
 
+import * as i18n from 'i18n';
+
 export class ErrorHandler {
     public static handleE11000(error, res, next): void {
         if (error.name === 'MongoError' && error.code === 11000) {
-            next(new Error('There was a duplicate key error'));
+            next(new Error(i18n.__('There was a duplicate key error')));
         } else {
             next();
         }
