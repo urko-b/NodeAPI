cb9d9d39caf18828ede694f7710485327e43f723
unknown <lordguerrero94@gmail.com>
2019-03-30 20:14:08 +0100

    test.helper.ts --> added some tsdoc comments

--- test/test.helper.ts
+++ test/test.helper.ts
@@ -1,8 +1,10 @@
 import * as mongoose from 'mongoose'
 export class TestHelper {
+  /**
+   * @remarks
+   * Remove each mongoose model registered
+   */
   public static removeMongooseModels = () => {
-    // Remove each mongoose model because we need to
-    //  add each again using RoutesHandler.init function
     for (const key in mongoose.connection.models) {
       if (mongoose.connection.models.hasOwnProperty(key)) {
         delete mongoose.connection.models[key]
@@ -10,10 +12,18 @@ export class TestHelper {
     }
   }
 
+  /**
+   * @remarks
+   * Remove collections_schemas from mongoose models
+   */
   public static removeMongooseModelCollectionsSchemas = () => {
     delete mongoose.connection.models.collections_schemas
   }
 
+  /**
+   * @remarks
+   * Remove audit_log from mongoose models
+   */
   public static removeMongooseModelAuditLog = () => {
     delete mongoose.connection.models.audit_log
   }
